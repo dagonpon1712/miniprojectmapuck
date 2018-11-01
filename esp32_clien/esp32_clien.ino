9dfae94424fa063e81b4a0f6a30485b90a99d890
dagonpon1712 <44660892+dagonpon1712@users.noreply.github.com>
2018-11-01 11:23:38 +0700

    Add files via upload

---
+++ esp32_clien/esp32_clien.ino
@@ -0,0 +1,51 @@
+#include<WiFi.h>
+
+#define WIFI_STA_NAME "iPhone"
+#define WIFI_STA_PASS "pondkunk1712"
+
+WiFiServer server(23);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_STA_NAME);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_STA_NAME, WIFI_STA_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  }
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+}
+
+void loop() {
+  WiFiClient client = server.available();
+
+  if (client) {
+    Serial.println("new client");
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+      }
+      delay(1);
+    }
+    client.stop();
+    Serial.println("client disonnected");
+  }
+  delay(10);
+}
